b5def51230fccb41f18e744621e81af30878a852
GwenRaq <gwen.raq@gmail.com>
2017-03-17 09:03:10 -1000

    3/17

---
+++ js/app.js
@@ -0,0 +1,105 @@
+// var jsonDataBody = [
+// 	{
+// 		"header":"font"
+// 	},
+// 	{
+// 		"div": {
+// 			"class": "Neko",
+// 		}
+// 	{
+// 		"div":{
+// 			"class": "allpost",
+// 		}
+// 	{
+// 		"div":{
+// 			"class": "content",
+// 			"id": "first"
+// 			[
+// 			{"h2": {"My First Blog Entry"}}, 
+// 			{"h6": {"Aug 15, 2016"}}, 
+// 			"p": {"I took this class because I want to be a Computer Engineer. I wanted to experience things that a computer can do like coding. I was also interested in coding when I heard about it from a field trip and when I heard it in the Library."}, 
+// 			"p": {"Something that I want to get out of this class is that I want to be able to know all the basics of coding. I also want to know how to use coding programs like Java. I also want to be able to acomplish a project. For example, making a project or an app."}
+// 			]
+// 		}
+// 	{
+// 		"div":{
+// 			"class": "content",
+// 			"id": "second",
+// 			[
+// 			"h2": "HTML",
+// 			"h6": "August 23,2016",
+// 			"p": "I learned how to make an html website. I made the website with Sublime Text and Git Shell. First, I learned the git commands which are git add, git commit -m, git status, and git push. Git add means your adding a specific file to sublime text. Git commit means you're initializing the project. Git status is for checking your projects status and git push means that you're uploading everything 	into your respository.",
+// 			"p": "Next, I learned some of the basics or components for putting a website together on sublime text. I leanrned how to make a paragraph element, a closing tag, a header, a block element, an inline element, how to add an image, and how to make a closing tag.",
+// 			]
+
+
+// 		}
+// 		}
+// 			}
+// 		}
+// 	}
+// ];
+var simplifiedJSON = [
+	{
+		"journalEntry": "1",
+		"date": "Aug 15, 2016",
+		"title": "My First Blog Entry",
+		"content": ["I took this class because I want to be a Computer Engineer. I wanted to experience things that a computer can do like coding. I was also interested in coding when I heard about it from a field trip and when I heard it in the Library.",
+		"Something that I want to get out of this class is that I want to be able to know all the basics of coding. I also want to know how to use coding programs like Java. I also want to be able to acomplish a project. For example, making a project or an app.",]
+	}
+	{
+		"journalEntry": "2",
+		"date": "August 23,2016",
+		"title": "HTML",
+		"content": ["I learned how to make an html website. I made the website with Sublime Text and Git Shell. First, I learned the git commands which are git add, git commit -m, git status, and git push. Git add means your adding a specific file to sublime text. Git commit means you're initializing the project. Git status is for checking your projects status and git push means that you're uploading everything 	into your respository.", "Next, I learned some of the basics or components for putting a website together on sublime text. I leanrned how to make a paragraph element, a closing tag, a header, a block element, an inline element, how to add an image, and how to make a closing tag."]
+	}
+	{
+		"journalEntry": "3",
+		"date": "August 24,2016",
+		"title": "UX vs UI",
+		"content": ["UX stands for User Experience. It enhances the inability between the user and a product. It makes the product more fun to use and interactable. It is what the user will interact with. It also requires organization.", "UI stands for User Interface. It is the graphical design of the site, the visual element, and what you will see in the site."]
+
+	}
+	{
+		"journalEntry": "4",
+		"date": "October 7, 2016",
+		"title": "Responsive and Mobile Design",
+		"headers": ["Responsive Design", "Mobile Design"],
+		"content": ["A responsive design is building a site to respond to multiple sizes also known as fluid design. So when you make the window smaller, the website will also become smaller to fit the window. It won't cut of any of its properties. An example of it would be Facebook, Google, and beta.rallyinteractive.com.", "A mobile design is a web app design for mobile devices. Media queries makes the design effective for a mobile. An example is Instagram. A Media Query is break points in css that tells your app to change according to screensize."]
+
+
+	}
+	{
+		"journalEntry": "5",
+		"date": "October 7, 2016",
+		"title": "DigimonGO",
+		"content": "I learned a lot of things while I was making the DigimonGo website. I learned how to change the font of the texts, how to change the color when you hover over a text, how to add a photo through css and that you can change the location of it on css. It was really fun making the DigimonGo website even though it kind of gave me stress. I was proud of myself when I finished it."
+
+	}
+	{
+		"journalEntry": "6"
+		"date": "October 7, 2016",
+		"title": "Media Queries",
+		"content": "I learned that media queries is the break points in css that tells your app to change according to screensize. It changes the width of the screen depending on how big or small your device is. I learned how to make it by making a family block. I think that I also had a lot of fun making this, seeing the colors change and blocks change when you make the screen smaller."
+
+	}	
+	{
+		"journalEntry": "7",
+		"date": "November 2, 2016",
+		"title": "Basic Javascript",
+		"content": "While I was learning the javascripts basics, it was really hard for me to understand. I wasn't sure what we were making. After a couple of practices I kind of understood a some of it but I'm still kind of lost. I learned how to make a string, a function, variables, etc. I also learned how to make a conditional statement which I think is really fun to read and make."
+
+	}
+	{
+		"journalEntry": "8",
+		"date": "November 2, 2016",
+		"title": "Conditional (Aliens)",
+		"content": "I was surprised that conditional statements were also used in coding. I thought it was only used in math or science. I was glad that I had some background ideas of what a conditional statement was through math team. It helped me a lot while Zedrick and I were solving the aliens assignment. I think if I never learned how to solve a conditional statement, I wouldn't have solved it. I had a lot of fun figuring out how many red, green, and blue aliens there were and I also had fun writing the conditional statement for java."
+	}
+	{
+		"journalEntry": "9",
+		"date": "November 28, 2016",
+		"title": "Verizon App Challenge Review",
+		"content": [""]
+	}
+]
---
+++ js/sudo.js
@@ -0,0 +1,15 @@
+/* 
+	Create a function named showAll 
+	Pass one parameter (data)
+	
+	Inside the finction
+	Iterate through data
+		for each object create elements
+		create parent 
+			with the id '#entry-' + object.id
+		create an h1 
+			append/add innerHTML object.title to h1
+			Repeat for every value
+			Append all elements + values to the parent div
+
+*/
